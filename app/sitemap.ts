3fa17b05f77f0714c56316db778c5055b6036a99
Evan Madden <evanjamesmadden@gmail.com>
2026-03-27 16:45:42 +0000

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://constructlyco.ie",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://constructlyco.ie/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://constructlyco.ie/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://constructlyco.ie/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://constructlyco.ie/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
